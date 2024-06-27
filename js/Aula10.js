0b9f8d727059ec7d6c98ef3112e155fff492dd3a
luciomotta <83682095+luciomotta@users.noreply.github.com>
2024-06-27 11:06:47 -0300

    Add files via upload

---
+++ js/Aula10.js
@@ -0,0 +1,37 @@
+function eventClick() {
+    //alert('Clicou em mim');
+    document.body.style.background = "yellow";
+}
+
+function eventoDbClick() {
+    alert('Evento de Click duplo')
+}
+
+function viraVermelho() {
+    let div = document.getElementById("test");
+    div.style.backgroundColor= "red";
+}
+function viraAzul() {
+    let div = document.getElementById("test");
+    div.style.backgroundColor= "Blue";
+}
+
+function AddTexto() {
+    let TextArea = document.getElementById("test02");
+    TextArea.append('o MOUSE SE MOVEU!!! <br>');
+}
+
+function trocaColorInput() {
+    let input = document.getElementById("input");
+    input.style.backgroundColor = "Green";
+    input.value=" [Focado troca a cor do input (onfocus())]";
+}
+
+function mudou() {
+    let select = document.getElementById("list");
+    let ValorSelecionado = select.value;
+
+    let input = document.getElementById("input");
+    input.value = ValorSelecionado;
+
+}
---
+++ js/Aula11.js
@@ -0,0 +1,37 @@
+/* ARRAYS Os arrays JavaScript são usados para armazenar vários valores 
+em uma única variável. Diferente dos objetos, que funcionam com 
+propriedades ou "nomes" que você dá para os itens dentro dele, os arrays 
+não possuem propriedades. O item dentro dele é encontrado pela posição.
+Imagine um array como uma lista de itens, com controle, por ordem de posição dentro dele.
+
+Ex: const lista = ["arroz", "feijão", "macarrão", "leite"];
+
+A lista[0] (lista na posição 0) vai conter o valor "arroz". 
+
+A lista[1] (lista na posição 1) vai conter o valor "feijão".
+
+E assim por diante.
+*/
+
+var item1 = "arroz";
+var item2 = "faijão";
+var item3 = "Macarrão";
+var item4 = "leite";
+
+                //0        //1      //2         //3 
+const lista = ["arroz", "feijão", "Macarrão", "leite"];
+console.log(lista[0]);
+
+// Comecei com ele faziu e depois atribuiu os valores!!
+const Mercado = []
+Mercado[0] = "BigBox";
+Mercado[1] = "DiaDia";
+Mercado[2] = "Carrefoul";
+Mercado[3] = "Tatico";
+
+console.log(Mercado);
+
+
+
+
+
